78d2b44ca296cbc27625e383b6b52aa017a65848
NoeKen <kenfaclnoe@gmail.com>
2026-09-07 20:27:22 -0400

    refactor(web): retire stale vendor client methods

--- src/features/vendors/index.ts
+++ src/features/vendors/index.ts
@@ -1,5 +1,4 @@
 export { vendorsService } from "./services/vendors.service";
 export { vendorRequestsService } from "./services/vendor-requests.service";
 export type { VendorRequest, CreateVendorRequestPayload, RespondVendorRequestPayload } from "./services/vendor-requests.service";
-export { useVendors } from "./hooks/useVendors";
-export type { Vendor, VendorCategory, CreateVendorInput } from "./types";
+export type { Vendor, VendorCategory } from "./types";
